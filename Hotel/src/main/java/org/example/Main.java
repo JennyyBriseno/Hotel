aaf4e74cee0711483b8f0887631e21cc653512d4
JennyyBriseno <Jennybriseno12@gmail.com>
2024-05-07 14:41:07 -0500

    addedLine

--- Hotel/src/main/java/org/example/Main.java
+++ Hotel/src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Add some code");
+        System.out.println("Adding another line of code.");
     }
 }
