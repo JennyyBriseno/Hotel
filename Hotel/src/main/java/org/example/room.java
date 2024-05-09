96049e4d45e50f8bb99206f1d93af780cee4bf97
JNL001O <jlukusa@appdev.yearup.org>
2024-05-09 12:54:00 -0500

    CreateRoomClass

--- Hotel/src/main/java/org/example/room.java
+++ Hotel/src/main/java/org/example/room.java
@@ -1,49 +1,44 @@
 package org.example;
 
 public class room {
-    private boolean isDirty;
-    private double price;
-    private boolean isAvailable;
     private int numberOfBeds;
+    private double price;
+    private boolean Occupied;
+    private boolean dirty;
 
-    public boolean isDirty() {
-        return isDirty;
+    //Constructor
+    public room(int numberOfBeds, double price, boolean occupied, boolean dirty){
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.Occupied = occupied;
+        this.dirty = dirty;
     }
+     public int getNumberOfBeds(){
+        return numberOfBeds;
+     }
+     public void setNumberOfBeds(int numberOfBeds){
+        this.numberOfBeds = numberOfBeds;
+     }
+     public double getPrice(){
+        return price;
+     }
+     public void setPrice(double price){
+        this.price = price;
+     }
+     public boolean isOccupied(){
+        return Occupied;
+     }
+     public void setOccupied(boolean occupied){
+        this.Occupied = occupied;
+     }
+     public boolean isDirty(){
+        return dirty;
+     }
 
     public void setDirty(boolean dirty) {
-        isDirty = dirty;
+        this.dirty = dirty;
     }
-
-    public double getPrice() {
-        return price;
+    public boolean isAvailable(){
+        return ! Occupied && ! dirty;
     }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    public int getNumberOfBeds() {
-        return numberOfBeds;
-    }
-
-    public void setNumberOfBeds(int numberOfBeds) {
-        this.numberOfBeds = numberOfBeds;
-    }
-
-    public room(boolean isDirty, double price, boolean isAvailable, int numberOfBeds){
-        this.isDirty = isDirty;
-        this.isAvailable =isAvailable;
-        this.numberOfBeds =numberOfBeds;
-        this.price = price;
-    }
-
-
 }
